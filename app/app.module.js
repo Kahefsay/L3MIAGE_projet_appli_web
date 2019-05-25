6205b7cd0592e96348afe6b3109f0ce52faa4406
Unknown <damian.riquart@hotmail.com>
2019-05-26 00:13:37 +0200

    traduction la miage + navbar

--- app/app.module.js
+++ app/app.module.js
@@ -1,6 +1,7 @@
 var app = angular.module('MiageToulouse', [
     'ngRoute',
     'ngMessages',
+    'pascalprecht.translate',
     'mdl',
     'footercustom',
     'accueil',
@@ -23,8 +24,26 @@ var app = angular.module('MiageToulouse', [
     'questionService'
 ]);
 
+app.config(function ($translateProvider) {
+    $translateProvider
+        .useStaticFilesLoader({
+            prefix: '/assets/translate/',
+            suffix: '.json'
+        })
+        .useSanitizeValueStrategy('sanitizeParameters')
+        .preferredLanguage('fr');
+});
+
+app.filter("htmlSafe", ['$sce', function ($sce) {
+    return function (htmlCode) {
+        return $sce.trustAsHtml(htmlCode);
+    };
+}]);
+
 app.run(['$rootScope', '$location',
     function ($rootScope) {
+        $rootScope.lang = 'fr';
+
         $rootScope.$on('$routeChangeError', function (event, next, previous, error) {
             if (error == 'AUTH_REQUIRED') {
                 alert("Vous devez être connecté pour accéder à cette page.");
@@ -37,7 +56,14 @@ app.run(['$rootScope', '$location',
     }
 ]);
 
-app.controller('mainCtrl', function ($scope, $http, $location, $utilisateurCourant) {
+app.controller('mainCtrl', function ($rootScope, $scope, $http, $location, $utilisateurCourant, $translate) {
+
+    console.log($rootScope.lang);
+
+    $scope.changeLanguage = function (key) {
+        $rootScope.lang = key;
+        $translate.use(key);
+    };
 
     if ($scope.user == null) {
         $utilisateurCourant.estConnecte($http)
